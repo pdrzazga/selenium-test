175796e64db1c0af91a9d35b6869b542bd0f103b
Paulina.Drzazga <Paulina.Drzazga@netent.com>
2020-04-17 19:06:57 +0200

    adding gmail test

---
+++ src/GmailTest.java
@@ -0,0 +1,32 @@
+import com.sun.nio.file.ExtendedWatchEventModifier;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+public class GmailTest {
+    public static void main(String[] args) {
+        String url = "http://gmail.com";
+
+        //run driver
+        SafariDriver driver = new SafariDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+
+        //insert user
+        WebElement user = driver.findElement(By.id("identifierId"));
+        user.sendKeys("paulina.test.selenium");
+        driver.findElement(By.cssSelector("span[class='RveJvd snByac']")).click();
+
+        //insert password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("psswrd123");
+        driver.findElement(By.cssSelector("svg[class='stUf5b']")).click();
+    }
+}
