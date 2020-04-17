fdaaf3adb1bc627e69cda8f82a28067abf7d68e8
Paulina.Drzazga <Paulina.Drzazga@netent.com>
2020-04-17 19:42:47 +0200

    user + password enter

--- src/GmailTest.java
+++ src/GmailTest.java
@@ -9,24 +9,39 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
+import java.util.concurrent.TimeUnit;
 
 public class GmailTest {
     public static void main(String[] args) {
-        String url = "http://gmail.com";
 
-        //run driver
-        SafariDriver driver = new SafariDriver();
-        driver.manage().window().maximize();
-        driver.get(url);
+            String url = "http://gmail.com";
 
-        //insert user
-        WebElement user = driver.findElement(By.id("identifierId"));
-        user.sendKeys("paulina.test.selenium");
-        driver.findElement(By.cssSelector("span[class='RveJvd snByac']")).click();
+            //run driver
+            SafariDriver driver = new SafariDriver();
+            driver.manage().window().maximize();
+            driver.get(url);
+
+            //insert user
+            WebElement user = driver.findElement(By.id("identifierId"));
+            user.sendKeys("paulina.test.selenium");
+            driver.findElement(By.cssSelector("span[class='RveJvd snByac']")).click();
+            delay();
 
         //insert password
-        WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("psswrd123");
-        driver.findElement(By.cssSelector("svg[class='stUf5b']")).click();
+            WebElement password = driver.findElement(By.id("password"));
+            password.sendKeys("psswrd123");
+            //driver.findElement(By.cssSelector("svg[class='stUf5b']")).click();
+
+    }
+
+    static void delay(){
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 }
